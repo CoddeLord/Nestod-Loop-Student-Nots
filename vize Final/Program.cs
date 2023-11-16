dc682b9882d574bfa76ec1ee34dd729d0714e892
CoddeLord <68156621+CoddeLord@users.noreply.github.com>
2023-11-16 15:27:35 +0300

    Add project files.

---
+++ vize Final/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+for (int i = 1; i <=4; i++)
+{
+  
+    Console.WriteLine(i + " .Öğrenci");
+   
+    for (int j = 1; j <= 3; j++)
+    {
+        Console.WriteLine(j + " .Ders");
+
+        Console.Write("Vize Notunuz = ");
+        int vize = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Final Notunuz = ");
+        int final = Convert.ToInt32(Console.ReadLine());
+        double ort = (vize* 0.4)+ (final * 0.6);
+        Console.WriteLine(j + " .Dersinizin Ortalaması " + ort);
+
+    }
+    Console.WriteLine("----------------");
+}
